8c544395466533d681739906acbb60195607dc0e
Nirmal-Singh-G <nirmalsinghg.10@gmail.com>
2026-04-21 10:30:29 +0530

    Third Message

--- EachWordReverse.js
+++ EachWordReverse.js
@@ -6,3 +6,16 @@ function reverseEachWord(str) {
 }
 
 console.log(reverseEachWord("hello world")); // olleh dlrow
+
+//Largest Number in Array
+
+let arr = [10, 5, 20, 8];
+let max = arr[0];
+
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+        max = arr[i];
+    }
+}
+
+console.log(max); // 20
