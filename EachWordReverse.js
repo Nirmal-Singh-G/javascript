ce8bbde27b60900126581e55782d94f3480941bb
Nirmal-Singh-G <nirmalsinghg.10@gmail.com>
2026-04-21 10:14:37 +0530

    Second Message

--- EachWordReverse.js
+++ EachWordReverse.js
@@ -6,3 +6,13 @@ function reverseEachWord(str) {
 }
 
 console.log(reverseEachWord("hello world")); // olleh dlrow
+
+//palindrome
+let str = "madam";
+let reversed = str.split("").reverse().join("");
+
+if (str === reversed) {
+    console.log("Palindrome");
+} else {
+    console.log("Not Palindrome");
+}
